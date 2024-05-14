602ee8c10f4c5115f99536266785d42f1a809029
TanayD304 <tdubey304@gmail.com>
2024-05-14 08:58:53 +0530

    Completed single data object functions
    Endpoints implemented -
     - Patient Info
     - Kidney Info
     - Add a kidney
     - Treat kidneys
     - Remove unhealthy kidneys

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let user1 = {
   fname: "Tanay",
   lname: "Dubey",
@@ -19,6 +21,10 @@ let user1 = {
 };
 
 app.get('/patient-info', (req, res) => {
+    res.json(user1)
+})
+
+app.get('/kidney-info', (req, res) => {
     const patientName = user1.fname + " " + user1.lname
     let healthyKidneys = 0;
     for(let i=0;i<user1.kidneys.length;i++) {
@@ -32,23 +38,56 @@ app.get('/patient-info', (req, res) => {
 })
 
 app.post('/add-new-kidney', (req, res) => {
-    if(user1.kidneys.length<2) {
-        console.log("Hello")
-        // TODO: Add this Functionality
+    let kidney = {
+        kidneyPos:"Left",
+        isHealthy:req.body.isHealthy
+    }
+    if(user1.kidneys.length==0) {
+        user1.kidneys.push(kidney)
+        res.status(200).json({
+          msg: "Kidney Added Successfully",
+        });
+    } else if (user1.kidneys.length==1) {
+        if(user1.kidneys[0].kidneyPos=='Left') {
+            kidney.kidneyPos = "Right"
+            user1.kidneys.push(kidney);
+        } else {
+            user1.kidneys.push(kidney);
+        }
+        res.status(200).json({
+          msg: "Kidney Added Successfully",
+        });
     } else {
-        res.status(413).json({
-            msg:"Patient already has 2 kidneys"
-        })
+      res.status(413).json({
+        msg: "Patient already has 2 kidneys",
+      });
     }
 })
 
 
 app.put('/treating-a-kidney', (req, res) => {
-    // TODO: Add this functionality
+    const position = req.query.pos
+
+    if(user1.kidneys[0].kidneyPos==position) {
+        user1.kidneys[0].isHealthy = true
+    } else {
+        user1.kidneys[1].isHealthy = true;
+    }
+    res.json(user1)
 })
 
-app.delete('/removing-a-kidney', (req, res) => {
-  // TODO: Add this functionality
+app.delete('/removing-unHealthy-kidney', (req, res) => {
+    for(let i=0;i<user1.kidneys.length;i++) {
+        if(user1.kidneys[i].isHealthy==false) {
+            if(i==0) {
+                user1.kidneys.shift();
+                i--;
+            } else {
+                user1.kidneys.pop();
+            }
+        }
+    }
+    res.json(user1)
 })
 
 app.listen(3004)
